ff8bfb10ed0b8282fe69cb413e84790b1aa625b1
eang <tawirath@gmail.com>
2021-03-26 22:12:42 +0700

    [add]edit

---
+++ src/pages/Basket.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Basket() {
+    return (
+        <div className ="container">
+            <video autoPlay loop className="videobackground" src="Coffee - 4947.mp4"></video>
+        </div>
+    )
+}
+
+export default Basket
